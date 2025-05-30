544e0c4341a0f705210247ee474c2525eda0464f
AntonLueders <145451222+AntonLueders@users.noreply.github.com>
2025-05-30 14:12:26 +0200

    Update CalcVelocity.m

--- CalcVelocity.m
+++ CalcVelocity.m
@@ -1,6 +1,8 @@
+% Approximates the angular velocity of the moments of the slider. The
+% result is given to "CalcTorque" for the calculation of the shaft
+% friction.
 function [velocity] = CalcVelocity(magnet_array_0, magnet_array_1, ...
     time_step)
 
-    velocity = (magnet_array_1 - magnet_array_0) / time_step;
-    
+    velocity = (magnet_array_1 - magnet_array_0) / time_step;    
 end
