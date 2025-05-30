1f3c8aba0ab85c42a6f760122a9713548466fb53
AntonLueders <145451222+AntonLueders@users.noreply.github.com>
2025-05-30 14:14:49 +0200

    Update Move.m

--- Move.m
+++ Move.m
@@ -1,3 +1,5 @@
+% Integration step of the equations of motion of the slider spins. 
+% The code is based on the Verlet algorithm known from MD simulations.
 function [angle_0, angle_1] = Move(magnet_array_0, magnet_array_1, ...
     torques, time_step, moment_of_inertia)
 
